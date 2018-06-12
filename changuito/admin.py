72fd722f676edc72c09c6f927299ed17b06783d0
bertini36 <albertopouquiros@gmail.com>
2018-06-12 12:15:22 +0200

    Problem with related names

--- changuito/admin.py
+++ changuito/admin.py
@@ -2,18 +2,25 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from django.contrib import admin
-
 from changuito.models import Cart
+from django.conf import settings
+from django.contrib import admin
 
 from .models import Item
 
+try:
+    cart_model = settings.CART_MODEL
+except AttributeError:
+    from .models import Cart
 
-class CartAdmin(admin.ModelAdmin):
-    pass
+
+if not cart_model:
+
+    class CartAdmin(admin.ModelAdmin):
+        pass
 
 
-admin.site.register(Cart, CartAdmin)
+    admin.site.register(Cart, CartAdmin)
 
 
 class ItemAdmin(admin.ModelAdmin):
